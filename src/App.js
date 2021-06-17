8f027c5d2617fc1ecef109f6b24b0cc7d0b4484a
erkansevm <erkansevm528@gmail.com>
2021-06-17 17:46:04 +0300

    get last commit link

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import helpers from './helpers/index';
 import './App.scss';
 
 function App() {
   const [repoLink, setRepoLink] = useState('');
-  const [repo,setRepo] = useState({});
-  const [submited, setSubmited] = useState(false);
-  const [branches, setBranches] = useState([]);
-  const [mainBranch, setMainBranch] = useState({})
+  const [localStates, setLocalStates] = useState({
+    repo:{},
+    branches:[],
+    selectedBranch:'',
+    selectedCommitOption:'last-commit',
+  })
+  const [steps, setSteps] = useState({
+    submited:false,
+    final:false,
+  })
+  const [result,setResult] = useState(''); 
+
+  useEffect(() => {
+    async function calculateLink() {
+      if (steps.final === true) {
+        const link = await helpers.findCommits(repoLink,localStates.selectedBranch,localStates.selectedCommitOption);
+        setResult(link);
+      }
+    }
+
+    calculateLink();
+  },[localStates.selectedBranch,localStates.selectedCommitOption,repoLink,steps.final])
   
   const repoOnSubmit = async (e) => {
     e.preventDefault();
-    console.log('submited');
-    const repoResponse =  await helpers.fetchUrl(helpers.createRootReqUrl(repoLink));
-    const branchesResponses = await helpers.fetchUrl(repoResponse.data.branches_url.replace('{/branch}',''));
-    setRepo(repoResponse);
-    setBranches(branchesResponses.data);
-    setSubmited(true);
+    const repoResponse = await helpers.fetchUrl(helpers.createRootReqUrl(repoLink));
+    const branchesResponses = await helpers.fetchUrl(repoResponse.data.branches_url.replace('{/branch}', ''));
+    setLocalStates({
+      ...localStates,
+      repo: repoResponse.data,
+      branches: branchesResponses.data,
+      selectedBranch:branchesResponses.data[0].name
+    })
+    setSteps({
+      ...steps,
+      submited:true
+    });
   }
+
+  const resultInfo = steps.submited ? 'One more step.Select branch and commit options': "Copy repo link and press enter for options";
+
   return (
     <div className="app">
       <div className="content">
         <div className="url-input element">
           <i className="fab fa-github"></i>
-          <form
-            onSubmit={repoOnSubmit}
-          >
+          <form onSubmit={repoOnSubmit}>
             <input
               value={repoLink}
               placeholder="Your url"
@@ -35,26 +60,83 @@ function App() {
             />
           </form>
         </div>
-        {
-          submited ? 
+        {steps.submited ? (
           <>
-          <div className="element branch-options">
-         
-                <select id="branches">
-                {branches.map((branch,index) => {
-                    return <option value={branch.name} key={index}>{branch.name}</option>
-                  })}
-                </select>
-                <button type="submit"></button>
-          </div>
-         
-          </> : null
-        }
+            <div className="element branch-options">
+              <select
+                defaultValue={localStates.branches[0].name}
+                onChange={(e) => {
+                  setLocalStates({
+                    ...localStates,
+                    selectedBranch: e.target.value
+                  })
+                }}
+                id="branches"
+              >
+                {localStates.branches.map((branch, index) => {
+                  return (
+                    <option value={branch.name} key={index}>
+                      {branch.name}
+                    </option>
+                  );
+                })}
+              </select>
+              <button type="submit"></button>
+            </div>
+            <div className="element branch-options">
+              <select
+                value='last-commit'
+                onChange={(e) => {
+                  setLocalStates({
+                    ...localStates,
+                    selectedCommitOption: e.target.value,
+                  })
+                }}
+                id="commits"
+              >
+                <option  value='last-commit'>
+                        Last commit
+                </option>
+                <option  value='first-commit'>
+                        First commit
+                </option>
+                
+              </select>
+            </div>
+            <div className=" final-button-wrapper">
+              <button 
+                className="final-result"
+                onClick = {
+                  e => {
+                    console.log(repoLink);
+                    setSteps({
+                      ...steps,
+                      final: true
+                    });
+                  }
+                }
+              >
+                Get link!
+              </button>
+            </div>
+           
+          </>
+        ) : null}
 
         <div className="result element">
           <div className="result-inner">
             <div className="wrapper">
-              <a target="_blank" rel="noreferrer" href={helpers.findBranches(repoLink)}>{helpers.findBranches(repoLink)}</a>
+              {steps.final ? (
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={result}
+                >
+                  {result}
+                </a>
+              ) : (
+                resultInfo
+              )}
             </div>
           </div>
         </div>
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,18 +7,71 @@ const helpers = {
     const repoOwner = info[info.length-2];
     return `https://api.github.com/repos/${repoOwner}/${repoName}`;
   },
-  findBranches : function (repoLink) {
-    return 'https://'+this.createRootReqUrl(repoLink) + '/branches';
+  findCommitCountAtGivenBranch :  function (repoLink,branchName,option) {
+/*     //https://api.github.com/repos/vitejs/vite/commits?sha=main
+    const url = `${this.createRootReqUrl(repoLink)}/commits?sha=${branchName}`;
+    console.log('url-commit',url);
+    const response = await axios.get(url);
+    console.log('commits',response.data);
+    return repoLink+ '/commits/'+branchName; */
+    let url = `${this.createRootReqUrl(repoLink)}/commits?sha=${branchName}&per_page=100`
+    let pages = 0;
+
+  return fetch(url, {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+      },
+    })
+    .then((data) => data.headers)
+    .then(
+      (result) =>
+      result
+      .get("link")
+      .split(",")[1]
+      .match(/.*page=(?<page_num>\d+)/).groups.page_num
+    )
+    .then((numberOfPages) => {
+      pages = numberOfPages;
+      return fetch(url + `&page=${numberOfPages}`, {
+        headers: {
+          Accept: "application/vnd.github.v3+json",
+        },
+      }).then((data) => data.json());
+    })
+    .then((data) => {
+      return data.length + (pages - 1) * 100;
+    })
+    .catch((err) => {
+      console.log(`ERROR: calling: ${url}`);
+      console.log("See below for more info:");
+      console.log(err);
+    });
   },
-  findCommits: function (repoLink) {
-    return this.createRootReqUrl(repoLink) + '/commits';
+  findCommits:async function (repoLink,branchName,option) {
+    const commitCount = await this.findCommitCountAtGivenBranch(repoLink, branchName, option);
+    if (commitCount < 35) {
+      return `${repoLink}/commits/${branchName}`
+    }
+
+    const numberAfterSha = commitCount - 35;
+    let url = `${this.createRootReqUrl(repoLink)}/commits?sha=${branchName}&per_page=35`
+    const response = await axios.get(url);
+    const lastCommitsShaofPage = response.data[0].sha;
+    console.log(response);
+    console.log(lastCommitsShaofPage);
+
+    let resultUrl = `${repoLink}/commits/${branchName}?after=${lastCommitsShaofPage}+${numberAfterSha}&branch=${branchName}`;
+    return resultUrl;
   },
   fetchUrl:  async function (requestUrl) {
    console.log('url',requestUrl);
    const response = await axios.get(requestUrl);
    console.log(response);
    return response;
-  }
+  },
+  
 }
 
 export default helpers;
+
+  
